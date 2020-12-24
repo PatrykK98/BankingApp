f29cbd5dba3c0673ea13d27a2979f99a95883afd
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2020-12-24 10:04:24 +0100

    Add files via upload

---
+++ restricted/CreditAcc.java
@@ -0,0 +1,43 @@
+package restricted;
+
+import java.math.BigDecimal;
+
+import ExceptionClasses.ReachedCreditLimitException;
+
+public class CreditAcc extends Account {
+	BigDecimal limit;
+
+	public CreditAcc(String number, BigDecimal limit) {
+		super(number);
+		this.limit=limit;
+	}
+	@Override
+	BigDecimal withdraw(BigDecimal amount) throws ReachedCreditLimitException  {
+		balance = balance.subtract(amount);
+		if (balance.compareTo(limit) == 0){
+			throw new ReachedCreditLimitException("You reached the limit which is: "+limit);
+		}
+		if (balance.compareTo(limit) == -1){
+			balance = balance.add(amount);
+			throw new ReachedCreditLimitException("Can't make the transaction bacause you reached the limit, which is: "+limit+"\nYour balance is: "+balance);
+		}
+		if (balance.compareTo(limit) == 1) {
+			System.out.println("Your balance is: ");
+			return balance;
+		}
+		return balance;
+	}
+	
+	@Override
+	BigDecimal recalculatePercents(BigDecimal percent) {
+		if(balance.compareTo(BigDecimal.ZERO) == 1) {
+		return balance.subtract(balance.multiply(percent));
+		}
+		else if((balance.compareTo(BigDecimal.ZERO) == -1)){
+			return balance.add(balance.multiply(percent));
+		}
+		return balance;
+	}
+	
+
+}
---
+++ restricted/DebitAcc.java
@@ -0,0 +1,42 @@
+package restricted;
+
+import java.math.BigDecimal;
+
+import ExceptionClasses.NonSufficientFundsException;
+
+public class DebitAcc extends Account {
+	private BigDecimal limit = BigDecimal.ZERO;
+
+	public DebitAcc(String number) {
+		super(number);
+	}
+
+	@Override
+	BigDecimal withdraw(BigDecimal amount) throws NonSufficientFundsException {
+		balance = balance.subtract(amount);
+		if (balance.compareTo(limit) == -1) {
+			balance = balance.add(amount);
+			throw new NonSufficientFundsException(
+					"You can't make the transaction beacause you do not have enough money \nYour balance is: "
+							+ balance);
+		}
+
+		if (balance.compareTo(limit) == 0) {
+			throw new NonSufficientFundsException("Your balance equals: " + balance);
+		}
+		if (balance.compareTo(limit) == 1) {
+			System.out.println("Your balance is: ");
+		}
+		return balance;
+	}
+
+	@Override
+	BigDecimal recalculatePercents(BigDecimal percent) {
+		BigDecimal value = balance.subtract(balance.multiply(percent));
+		if (balance.compareTo(BigDecimal.ZERO) == 1) {
+			return balance.subtract(balance.multiply(percent));
+		}
+		return balance;
+
+	}
+}

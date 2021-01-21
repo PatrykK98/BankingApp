ae2606766e92d49856187dfff261493f110580e4
PatrykK98 <p.krupa328@gmail.com>
2021-01-21 11:59:02 +0100

    A few test added.

--- src/BankApp.java
+++ src/BankApp.java
@@ -1,7 +1,3 @@
-import java.math.BigDecimal;
-
-import javax.security.auth.login.AccountNotFoundException;
-
 import ExceptionClasses.NonSufficientFundsException;
 import ExceptionClasses.ReachedCreditLimitException;
 import restricted.Account;
@@ -9,8 +5,13 @@ import restricted.Bank;
 import restricted.CreditAcc;
 import restricted.DebitAcc;
 
-public class BankApp {
+import javax.security.auth.login.AccountNotFoundException;
+import java.math.BigDecimal;
 
+public class BankApp {
+	static {
+		initBanking();
+	}
 	public static void main(String[] args) {
 		Bank lloyds = new Bank("Lloyds");
 		Bank alior = new Bank("Alior");
@@ -35,5 +36,20 @@ public class BankApp {
 		}catch(AccountNotFoundException b) {
 			System.out.println(b.getMessage());
 		}
+	}
+	private static void initBanking() {
+		Bank lloyds = new Bank("Lloyds");
+		Bank alior = new Bank("Alior");
+		Account personal = new CreditAcc("12345", new BigDecimal("-50"));
+		Account pers = new CreditAcc("543", new BigDecimal("-80"));
+		Account corporate = new DebitAcc("54321");
+		Account corp = new DebitAcc("654");
+		lloyds.registerCreditAccount(personal);
+		lloyds.registerCreditAccount(pers);
+		alior.registerDebitAccount(corporate);
+		alior.registerDebitAccount(corp);
+		NationalBank.registerBank(lloyds);
+		NationalBank.registerBank(alior);
+
 	}
 }
